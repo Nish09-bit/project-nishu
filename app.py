da7f6ced39a21d8804a2df9ea1e0db0a7c4d696e
Nish09-bit <nishthapathak77@gmail.com>
2026-05-08 11:36:20 +0530

    ready for deployment

--- app.py
+++ app.py
@@ -772,4 +772,5 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
